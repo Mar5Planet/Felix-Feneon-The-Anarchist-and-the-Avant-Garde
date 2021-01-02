db772a4bd441206ef8959aa37de825f762229f41
Mar5Planet <amustaf@email.arizona.edu>
2021-01-02 16:21:38 -0500

    images added to carousel

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -24,15 +24,15 @@ const Carousel = () => {
         <>
             <div className="scene">
                 <div style={transform} className="carousel">
-                    <div className="carousel__cell">1</div>
-                    <div className="carousel__cell">2</div>
-                    <div className="carousel__cell">3</div>
-                    <div className="carousel__cell">4</div>
-                    <div className="carousel__cell">5</div>
-                    <div className="carousel__cell">6</div>
-                    <div className="carousel__cell">7</div>
-                    <div className="carousel__cell">8</div>
-                    <div className="carousel__cell">9</div>
+                    <div className="carousel__cell"/>
+                    <div className="carousel__cell"/>
+                    <div className="carousel__cell"/>
+                    <div className="carousel__cell"/>
+                    <div className="carousel__cell"/>
+                    <div className="carousel__cell"/>
+                    <div className="carousel__cell"/>
+                    <div className="carousel__cell"/>
+                    <div className="carousel__cell"/>
                 </div>
             </div>
                 <button onClick={prevButton}>
