9f784fd6573291d108ec4db6fda2b14030a8277f
Mar5Planet <amustaf@email.arizona.edu>
2021-01-02 17:38:40 -0500

    version 1 completed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import Carousel from './Components/Carousel';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <div className="app-text">
+        <h1>Félix Fénéon: The Anarchist and the Avant-Garde</h1>
+        <a href="https://press.moma.org/wp-content/uploads/2020/06/MoMA_FelixFeneon_Checklist.pdf">Inspired by the MoMA Exhibit</a>
+      </div>
       <Carousel />
     </div>
   );
--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -24,23 +24,25 @@ const Carousel = () => {
         <>
             <div className="scene">
                 <div style={transform} className="carousel">
-                    <div className="carousel__cell"/>
-                    <div className="carousel__cell"/>
-                    <div className="carousel__cell"/>
-                    <div className="carousel__cell"/>
-                    <div className="carousel__cell"/>
-                    <div className="carousel__cell"/>
-                    <div className="carousel__cell"/>
-                    <div className="carousel__cell"/>
-                    <div className="carousel__cell"/>
+                    <a href="https://nga.gov.au/bonnard/detail.cfm?IRN=157446&MnuID=1" className="carousel__cell"> </a>
+                    <a href="https://gallica.bnf.fr/ark:/12148/bpt6k83575m" className="carousel__cell"> </a>
+                    <a href="https://gallica.bnf.fr/ark:/12148/bpt6k835779" className="carousel__cell"> </a>
+                    <a href="https://www.moma.org/collection/works/78734" className="carousel__cell"> </a>
+                    <a href="https://www.moma.org/collection/works/7600" className="carousel__cell"> </a>
+                    <a href="https://nga.gov.au/exhibition/masterpiecesfromparis/default.cfm?IRN=191253&BioArtistIRN=16097&MnuID=SRCH&ArtistIRN=16097&ViewID=2" className="carousel__cell"> </a>
+                    <a href="https://frenchaffair.com.au/chat-noir/" className="carousel__cell"> </a>
+                    <a href="https://www.metmuseum.org/art/collection/search/333990" className="carousel__cell"> </a>
+                    <a href="https://nga.gov.au/bonnard/detail.cfm?IRN=3635&MnuID=1" className="carousel__cell"> </a>
                 </div>
             </div>
+            <div className="btn-div">
                 <button onClick={prevButton}>
-                    Previous
+                    &#8592;
                 </button>
                 <button onClick={nextButton}>
-                    Next
+                    &#8594;
                 </button>
+            </div>
         </>
     )
 }
